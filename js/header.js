96dae65994d1a5f7c3eb880dfe6d2faab54acba2
SongCheaWon <asdasd09909@gmail.com>
2025-02-17 16:42:00 +0900

    하단 완성

--- js/header.js
+++ js/header.js
@@ -1,8 +1,21 @@
 window.addEventListener("load", function () {
   const li = this.document.querySelectorAll(".tab li");
-  li.forEach(function (list) {
-    list.addEventListener("click", () => {
-      list.classList.toggle(".active");
+  const box = this.document.querySelectorAll(".item");
+  li[0].classList.add("active");
+  box[0].classList.add("active")
+  li.forEach(function (item, index) {
+    item.addEventListener("click", () => {
+      showbox(index);
     });
   });
+  function showbox(index) {
+    li.forEach(function (item) {
+      item.classList.remove("active");
+    });
+    box.forEach(function (item) {
+      item.classList.remove("active");
+    });
+    li[index].classList.add("active")
+    box[index].classList.add("active")
+  }
 });
