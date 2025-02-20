79404f79dc496434645cb96c01f250112fc22c55
hyobeen <a01035083619@gmail.com>
2025-02-20 14:21:38 +0900

    스크롤탑 가져가세영

--- js/header.js
+++ js/header.js
@@ -2,7 +2,7 @@ window.addEventListener("load", function () {
   const li = this.document.querySelectorAll(".tab li");
   const box = this.document.querySelectorAll(".item");
   li[0].classList.add("active");
-  box[0].classList.add("active")
+  box[0].classList.add("active");
   li.forEach(function (item, index) {
     item.addEventListener("click", () => {
       showbox(index);
@@ -15,7 +15,11 @@ window.addEventListener("load", function () {
     box.forEach(function (item) {
       item.classList.remove("active");
     });
-    li[index].classList.add("active")
-    box[index].classList.add("active")
+    li[index].classList.add("active");
+    box[index].classList.add("active");
   }
+  window.setTimeout(function () {
+    map.relayout();
+  }, 0);
+ 
 });
---
+++ js/scroll.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", function () {
+  const scrollTopBtn = this.document.querySelector(".scrollToTop");
+  const scrollDownBtn = this.document.querySelector(".scrollTodown");
+  scrollTopBtn.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+  scrollDownBtn.addEventListener("click", () => {
+    window.scrollTo({ top: 1000, behavior: "smooth" });
+  });
+});
