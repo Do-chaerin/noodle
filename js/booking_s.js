d4c81baf35552c20fb370fb5a941444632675bb7
Do-chaerin <swin3321@gmail.com>
2025-02-25 12:52:00 +0900

    이미지 슬라이더

--- js/booking_s.js
+++ js/booking_s.js
@@ -1,4 +1,4 @@
-function dxSimpleSlider(element = "#dx-slider", auto = false, pause) {
+function dxSimpleSlider(element = "#dx-slider", auto = false, pause = 3000) {
   var $this = $(element);
 
   var slidesCont = $this.children(".slides-container");
@@ -11,67 +11,64 @@ function dxSimpleSlider(element = "#dx-slider", auto = false, pause) {
   var nextSlide = arrowsCont.children(".next");
 
   var slidesCount = slides.length;
-  var currentSlide = slides.first();
-  var currentSlideIndex = 1;
+  var currentSlideIndex = 0;
   var autoPlay = null;
 
   // 모든 슬라이드를 숨기고 첫 번째 슬라이드만 보이게 설정
-  slides.hide();
-  currentSlide.show().addClass("active");
+  slides.css({ position: "absolute", top: 0, left: 0 }).hide();
+  slides.eq(currentSlideIndex).show().addClass("active");
+
+  // 초기 pager 업데이트
+  pager.text(currentSlideIndex + 1 + " / " + slidesCount);
+
+  function showSlide(newIndex) {
+    if (newIndex === currentSlideIndex) return; // 동일한 슬라이드로 이동 방지
+
+    var currentSlide = slides.eq(currentSlideIndex);
+    var newSlide = slides.eq(newIndex);
+
+    currentSlide.css("z-index", 1).fadeOut(500, function () {
+      $(this).removeClass("active");
+    });
+
+    newSlide.css("z-index", 2).fadeIn(500).addClass("active");
+
+    currentSlideIndex = newIndex;
+    pager.text(currentSlideIndex + 1 + " / " + slidesCount);
+  }
 
   function showNext() {
-    currentSlide.removeClass("active").hide(); // 현재 슬라이드 숨김
-
-    if (currentSlideIndex == slidesCount) {
-      currentSlide = slides.first();
-      currentSlideIndex = 1;
-    } else {
-      currentSlideIndex++;
-      currentSlide = currentSlide.next();
-    }
-
-    currentSlide.addClass("active").show(); // 새로운 슬라이드 표시
-    pager.text(currentSlideIndex + " / " + slidesCount);
+    var nextIndex = (currentSlideIndex + 1) % slidesCount;
+    showSlide(nextIndex);
   }
 
   function showPrev() {
-    currentSlide.removeClass("active").hide(); // 현재 슬라이드 숨김
+    var prevIndex = (currentSlideIndex - 1 + slidesCount) % slidesCount;
+    showSlide(prevIndex);
+  }
 
-    if (currentSlideIndex == 1) {
-      currentSlide = slides.last();
-      currentSlideIndex = slidesCount;
-    } else {
-      currentSlideIndex--;
-      currentSlide = currentSlide.prev();
+  function startAutoPlay() {
+    if (auto) {
+      clearInterval(autoPlay);
+      autoPlay = setInterval(showNext, pause);
     }
-
-    currentSlide.addClass("active").show(); // 새로운 슬라이드 표시
-    pager.text(currentSlideIndex + " / " + slidesCount);
   }
 
-  function AutoPlay() {
-    clearInterval(autoPlay);
-    if (auto == true)
-      autoPlay = setInterval(function () {
-        showNext();
-      }, pause);
-  }
-
-  $(nextSlide).click(function (e) {
+  nextSlide.on("click", function (e) {
     e.preventDefault();
     showNext();
-    AutoPlay();
+    startAutoPlay();
   });
 
-  $(prevSlide).click(function (e) {
+  prevSlide.on("click", function (e) {
     e.preventDefault();
     showPrev();
-    AutoPlay();
+    startAutoPlay();
   });
 
-  AutoPlay();
+  startAutoPlay();
 }
 
-$(function () {
+$(document).ready(function () {
   dxSimpleSlider("#slider", true, 3000);
 });
