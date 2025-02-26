98ec30d1746c4d8664e7f105ba6195246ed6b821
Kim Jinhee <geenhankuk@gmail.com>
2025-02-26 12:54:07 +0900

    226 1253 커밋

--- js/js.swipergeen.js
+++ js/js.swipergeen.js
@@ -1,5 +1,5 @@
 $(function(){
-    var swiper = new Swiper(".mySwiper", {
+    var swiper1 = new Swiper(".mySwiper", {
         cssMode: true,
         navigation: {
           nextEl: ".swiper-button-next",
@@ -12,3 +12,20 @@ $(function(){
         keyboard: true,
       });
 })
+// 모바일 햄버거바 메뉴
+$(document).ready(function () {
+  //햄버거바 클릭
+  $("#hamburger-menu").click(function () {
+    $(".mobile-nav").animate({ right: 0 }, 300);
+  });
+  // 닫기
+  $("#close-menu").click(function () {
+    $(".mobile-nav").animate({ right: "-250px" }, 300);
+  });
+  //하위메뉴 토글
+  $(".mobile-nav .gnb > li > a").click(function () {
+    // 열린 .depth를 닫기
+    $(".mobile-nav .depth").stop().slideUp();
+    $(this).next(".mobile-nav .depth").stop().slideToggle();
+  });
+});
